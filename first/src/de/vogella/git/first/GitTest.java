696ac857127a2a281c89424ac97ca7963c877fe3
Juan Manuel Nieto Moreno <boguslandia@gmail.com>
2013-06-02 20:13:59 +0200

    Output message changed

--- first/src/de/vogella/git/first/GitTest.java
+++ first/src/de/vogella/git/first/GitTest.java
@@ -6,8 +6,6 @@ public class GitTest {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("Hola mundo");
-
+		System.out.println("Hola mundo con GitHub");
 	}
-
 }
