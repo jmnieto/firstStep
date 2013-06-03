499365c10d4f95a5fd8d1add8d24c43868c61812
Juan Manuel Nieto Moreno <boguslandia@gmail.com>
2013-06-03 12:46:37 +0200

    solve

--- first/src/de/vogella/git/first/GitTest.java
+++ first/src/de/vogella/git/first/GitTest.java
@@ -8,5 +8,7 @@ public class GitTest {
 	public static void main(String[] args) {
 		System.out.println("This output message was changed from the webside!! What will happen?");
 		System.out.println("What happen to the conflict");
+	public static void main(String[] args) {
+		System.out.println("What happen to the conflict");
 	}
 }
